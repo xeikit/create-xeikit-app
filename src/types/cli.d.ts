c6e6b1f6d13299e78c55d595f578ff8569f1bd36
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-05-20 23:38:48 +0900

    ✨ feat: add react-router template option

---
+++ src/types/cli.d.ts
@@ -0,0 +1,4 @@
+export interface TemplateOptions {
+  label: string;
+  value: string;
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import type { TemplateOptions } from '../types/cli';
+
 export const DEFAULT_REGISTRY = 'https://raw.githubusercontent.com/xeikit/starter/templates/templates' as const;
 export const DEFAULT_TEMPLATE_NAME = 'nuxt3' as const;
 export const PACKAGE_MANAGERS = {
@@ -8,4 +10,7 @@ export const PACKAGE_MANAGERS = {
   deno: undefined,
 } as const;
 export const PACKAGE_MANAGER_OPTIONS = Object.keys(PACKAGE_MANAGERS) as Array<keyof typeof PACKAGE_MANAGERS>;
-export const TEMPLATE_OPTIONS = [{ label: 'Nuxt3', value: 'nuxt3' }];
+export const TEMPLATE_OPTIONS = [
+  { label: 'Nuxt3', value: 'nuxt3' },
+  { label: 'React Router (framework)', value: 'react-router' },
+] satisfies TemplateOptions[];
