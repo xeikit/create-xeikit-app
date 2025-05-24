68aef76ecd6cbf723b13b16341e479c5954de9fe
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-05-24 20:02:05 +0900

    🔧 chore: add vitest package and config file

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  test: {
+    reporters: 'verbose',
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'clover', 'json'],
+      reportsDirectory: './coverage',
+    },
+  },
+});
